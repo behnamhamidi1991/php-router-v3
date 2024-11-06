06e57163006b9a436eaff9af25f330eed8c9125e
behnamhamidi1991 <eslp91@hotmail.com>
2024-11-06 13:37:16 +0330

    main

---
+++ Framework/middleware/Authorize.php
@@ -0,0 +1,26 @@
+<?php
+
+
+namespace Framework\Middleware;
+
+use Framework\Session;
+
+class Authorize {
+    
+    /**
+     * Check if user is authenticated
+     * 
+     * @return bool
+     */
+    
+    
+    /**
+     * Handle the user's request 
+     * 
+     * @param string $role
+     * @return bool
+     */
+    public function handle($role) {
+
+    }
+}
